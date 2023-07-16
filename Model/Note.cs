67aef899eac05e57c23f250d696de7bbe05f55be
Ket0o <kupregion3@gmail.com>
2023-07-17 00:24:27 +0700

    Добавьте файлы проекта.

---
+++ Model/Note.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Note
+    {
+        private string _title;
+        private string _text;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+            }
+        }
+
+        public Note() {}
+
+        public Note(string title, string text)
+        {
+            Title = title;
+            Text = text;
+        }
+    }
+}
