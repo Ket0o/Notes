c0bd8e1b7c1f5b6396b2bb3dd49f1d7eb5bfcbb4
Ket0o <kupregion3@gmail.com>
2023-07-18 02:19:36 +0700

    Create ViewModel and connect projects

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Base;
+
+namespace ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        private NoteViewModel _selectedNote;
+
+        public ObservableCollection<NoteViewModel> Notes { get; } =
+            new ObservableCollection<NoteViewModel>();
+
+        public NoteViewModel SelectedNote 
+        { 
+            get { return _selectedNote;} 
+            set
+            {
+                _selectedNote = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ ViewModel/NoteViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Base;
+using Model;
+
+namespace ViewModel
+{
+    public class NoteViewModel: BaseViewModel
+    {
+        public Note Note { get; } = new Note();
+
+        public string Titile
+        {
+            get => Note.Title;
+            set
+            {
+                Note.Title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Text
+        {
+            get => Note.Text;
+            set
+            {
+                Note.Text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public NoteViewModel()
+        {
+
+        }
+    }
+}
